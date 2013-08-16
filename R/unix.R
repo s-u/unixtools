2c8e13b169e5eff7bf1cb200607db826d843bc7a
Simon Urbanek <simon.urbanek@r-project.org>
2013-08-16 11:41:32 -0400

    add chown

--- R/unix.R
+++ R/unix.R
@@ -1,3 +1,4 @@
 set.user <- function(name, gid=NA) .Call(C_setuser, name, gid)
 user.info <- function(users = NULL) .Call(C_userinfo, users)
 set.tempdir <- function(path) invisible(.Call(C_setTempDir, path.expand(path)))
+chown <- function(paths, uid, gid = NA, follow.symlinks=TRUE) .Call(C_chown, path.expand(paths), uid, gid, follow.symlinks)
--- src/unix.c
+++ src/unix.c
@@ -1,4 +1,6 @@
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <grp.h>
 #include <pwd.h>
 #include <unistd.h>
 
@@ -35,7 +37,7 @@ SEXP C_userinfo(SEXP sName) {
     SEXP res;
     struct passwd *p;
     int pc = 1, n, i;
-    int *uid, *gid, *rn;
+    int *uid, *gid;
     SEXP name, realname, home, shell;
     if (TYPEOF(sName) == REALSXP) {
 	sName = PROTECT(coerceVector(sName, INTSXP));
@@ -71,5 +73,54 @@ SEXP C_userinfo(SEXP sName) {
     setAttrib(res, R_ClassSymbol, mkString("data.frame"));
     setAttrib(res, R_RowNamesSymbol, sName);
     UNPROTECT(pc);
+    return res;
+}
+
+SEXP C_chown(SEXP sFn, SEXP sUid, SEXP sGid, SEXP sFollow) {
+    SEXP res;
+    int i, n, *ok, uid = -1, gid = -1, follow = asInteger(sFollow);
+    if (TYPEOF(sFn) != STRSXP) Rf_error("invalid paths");
+    n = LENGTH(sFn);
+
+    if (TYPEOF(sUid) == STRSXP) {
+	if (LENGTH(sUid) == 1) {
+	    struct passwd *p = getpwnam(CHAR(STRING_ELT(sUid, 0)));
+	    if (!p) Rf_error("User '%s' not found", CHAR(STRING_ELT(sUid, 0)));
+	    uid = p->pw_uid;
+	    if (sGid == R_NilValue)
+		gid = p->pw_gid;
+	} else Rf_error("Invalid user specification, must be a string or numeric scalar");
+    } else if (sUid != R_NilValue)
+	uid = asInteger(sUid);
+    
+    if (TYPEOF(sGid) == STRSXP) {
+	if (LENGTH(sGid) == 1) {
+	    struct group *g = getgrnam(CHAR(STRING_ELT(sGid, 0)));
+	    if (!g) Rf_error("Group '%s' not found", CHAR(STRING_ELT(sGid, 0)));
+	    gid = g->gr_gid;
+	} else Rf_error("Invalid group specification, must be a string or numeric scalar");
+    } else if (sGid != R_NilValue)
+	gid = asInteger(sGid);
+
+    if (gid < 0 && uid < 0)
+	Rf_error("Either uid or gid must be set to valid values");
+
+    ok = LOGICAL(res = allocVector(LGLSXP, n));
+    for (i = 0; i < n; i++) {
+	int this_gid = gid, this_uid = uid;
+
+	/* if either uid or gid is not set, we have to get it from the existing file */
+	if (gid < 0 || uid < 0) {
+	    struct stat st;
+	    if (follow ? stat(CHAR(STRING_ELT(sFn, i)), &st) : lstat(CHAR(STRING_ELT(sFn, i)), &st)) {
+		ok[i] = FALSE;
+		continue;
+	    }
+	    if (gid < 0) this_gid = st.st_gid;
+	    if (uid < 0) this_uid = st.st_uid;
+	}
+
+	ok[i] = (follow ? chown(CHAR(STRING_ELT(sFn, i)), this_uid, this_gid) : lchown(CHAR(STRING_ELT(sFn, i)), this_uid, this_gid)) ? FALSE : TRUE;
+    }
     return res;
 }

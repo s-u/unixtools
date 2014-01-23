13da33ddb313b82e2a01cc0e8b858a2ed6ac2420
Simon Urbanek <simon.urbanek@r-project.org>
2014-01-23 15:08:43 -0500

    try to avoid R_CStackDir

--- src/stack.c
+++ src/stack.c
@@ -3,13 +3,13 @@
 
 extern uintptr_t R_CStackLimit;
 extern uintptr_t R_CStackStart;
-extern int R_CStackDir;
 
 SEXP C_stackinfo() {
   int dummy;
-  intptr_t usage = R_CStackDir * (R_CStackStart - (uintptr_t)&dummy);
+  intptr_t usage = R_CStackStart - (uintptr_t)&dummy;
   SEXP res = mkNamed(REALSXP, (const char *[]) { "used", "limit", "" });
+  if (usage < 0) usage = -usage;
   REAL(res)[0] = (double) usage;
-  REAL(res)[1] = (R_CStackLimit < 0) ? NA_REAL : ((double) R_CStackLimit);
+  REAL(res)[1] = (R_CStackLimit == -1) ? NA_REAL : ((double) R_CStackLimit);
   return res;
 }
